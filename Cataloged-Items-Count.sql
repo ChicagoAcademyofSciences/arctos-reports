24ad424fbb0819b3f9d816241cce14d4b5357c78
Erica Krimmel <ekrimmel@gmail.com>
2017-06-02 17:43:26 -0500

    sync

---
+++ Cataloged-Items-Count.sql
@@ -0,0 +1,12 @@
+﻿SELECT flat.collection_cde, COUNT (DISTINCT flat.guid)
+
+FROM
+   flat
+
+WHERE
+  flat.guid LIKE 'CHAS%' AND
+  (flat.partdetail LIKE '%in collection%' OR
+   flat.partdetail LIKE '%on exhibit%' OR
+   flat.partdetail LIKE '%on loan%')
+
+GROUP BY flat.collection_cde
--- Specimen-Part-Count.sql
+++ Specimen-Part-Count.sql
@@ -1,4 +1,4 @@
-﻿SELECT collection.collection_cde, SUM(specimenPartCollObject.lot_count)
+﻿SELECT collection.collection_cde, SUM(specimenPartCollObject.lot_count) NumberOfSpecimenParts
 
 FROM
    collection,   
@@ -10,6 +10,9 @@ WHERE
   collection.institution_acronym LIKE 'CHAS' AND
   collection.collection_id=cataloged_item.collection_id (+) AND
   cataloged_item.collection_object_id=specimen_part.derived_from_cat_item (+) AND
-  specimen_part.collection_object_id=specimenPartCollObject.collection_object_id (+)
+  specimen_part.collection_object_id=specimenPartCollObject.collection_object_id (+) AND
+  (specimenPartCollObject.coll_obj_disposition LIKE 'in collection' OR
+   specimenPartCollObject.coll_obj_disposition LIKE 'on exhibit' OR
+   specimenPartCollObject.coll_obj_disposition LIKE 'on loan')
 
-GROUP BY collection.collection_cden_object_id (+)
+GROUP BY collection.collection_cde
