212eec3fcf535b3bf041264ce16e5622943281f6
Erica Krimmel <ekrimmel@gmail.com>
2017-11-01 12:46:11 -0500

    rewrite query

--- find-specimens-with-multiple-storage-locations.sql
+++ find-specimens-with-multiple-storage-locations.sql
@@ -1,20 +1,15 @@
 SELECT
-    flat.collection_id,
-    flat.cat_num,
-    specimen_part.part_name,
-    specimen_part_attribute.attribute_value location,
-    COUNT(location) count
-    coll_object.lot_count lot_count,
-    coll_object.coll_obj_disposition
+    specimen_part.collection_object_id part,
+    COUNT(specimen_part_attribute.part_attribute_id) attribute_count
 FROM
     flat,
     specimen_part,
     specimen_part_attribute,
-    coll_object,
-    loan_item
+    coll_object
 WHERE
-    specimen_part.collection_object_id = coll_object.collection_object_id AND
-    specimen_part.collection_object_id = specimen_part_attribute.collection_object_id AND
-    specimen_part.derived_from_cat_item = flat.collection_object_id AND
-    flat.cat_num IN (SELECT flat.cat_num FROM flat
-GROUP BY flat.cat_num HAVING COUNT (flat.cat_num) >1)
+    flat.guid LIKE 'CHAS:Egg%' AND
+    flat.collection_object_id=specimen_part.derived_from_cat_item (+) AND
+    specimen_part.collection_object_id=coll_object.collection_object_id (+) AND
+    specimen_part.collection_object_id=specimen_part_attribute.collection_object_id (+)
+GROUP BY  specimen_part.collection_object_id
+ORDER BY attribute_count DESC
