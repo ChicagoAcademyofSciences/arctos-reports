c2b325080dfc7345296fd72eb6579d9a114de009
anna-chinn <achinn@naturemuseum.org>
2018-06-30 15:40:16 -0500

    CHAS Loan Header update

--- CHAS_loan_header.sql
+++ CHAS_loan_header.sql
@@ -1,3 +1,5 @@
+--Current CHAS CHAS-loan-header handler
+/*
 SELECT
   get_address(inside_contact.agent_id,'work phone') inside_phone_number
 FROM
@@ -8,3 +10,21 @@ WHERE
   loan.transaction_id = trans.transaction_id AND
   trans.transaction_id = inside_contact.transaction_id (+) AND
   loan.transaction_id=#transaction_id#
+*/
+
+--an attempt at fixing the handler- still doesn't work...
+select
+  (select address.address
+    from address
+    where trans_agent.trans_agent_role = 'outside contact' AND
+      address.address_type = 'work phone') outside_phone_number
+
+FROM
+  loan
+
+JOIN trans ON loan.transaction_id = trans.transaction_id
+JOIN trans_agent ON trans.transaction_id = trans_agent.transaction_id
+JOIN agent ON trans_agent.agent_id = agent.agent_id
+
+WHERE loan.transaction_id = #transaction_id#
+LIMIT 1
