c626475d1455d932aab187cecd3a6dc40dfe2e32
anna-chinn <achinn@naturemuseum.org>
2019-02-05 13:44:08 -0600

    tie tag updates

--- CHAS_tag.sql
+++ CHAS_tag.sql
@@ -1,5 +1,74 @@
 --use for CHAS_tag_front.cfr and CHAS_tag_back.cfr
+SELECT
+    scientific_name,
+    cat_num,
+    ACCESSION,
+    higher_geog,
+    SPEC_LOCALITY,
+    flat.VERBATIM_LOCALITY,
+    flat.COLLECTING_METHOD,
+    flat.COLLECTING_SOURCE,
+    flat.BEGAN_DATE,
+    flat.ENDED_DATE,
+    flat.VERBATIM_DATE,
+    COLL_EVENT_REMARKS,
+    round(dec_lat,5) dec_lat,
+    round(dec_long,5) dec_long,
+    COLLECTORS,
+    decode(trim(flat.sex),
+      'male','M',
+      'female','F',
+      'male ?','M?',
+      'female ?','F?',
+      '') sex,
+    ATTRIBUTES,
+    PREPARATORS,
+    PARTS,
+    REMARKS,
+    flat.MINIMUM_ELEVATION,
+    flat.ORIG_ELEV_UNITS,
+    flat.DATUM,
+    ConcatAttributevalue(flat.collection_object_id,'verbatim preservation date') as verbatim_preservation_date,
+    ConcatAttributevalue(flat.collection_object_id,'age class') as age_class,
+    ConcatAttributevalue(flat.collection_object_id,'unformatted measurements') as unformatted_measurements,
+    ConcatAttributevalue(flat.collection_object_id,'right gonad width') as right_gonad_width,
+    ConcatAttributevalue(flat.collection_object_id,'left gonad width') as left_gonad_width,
+    ConcatAttributevalue(flat.collection_object_id,'right gonad length') as right_gonad_length,
+    ConcatAttributevalue(flat.collection_object_id,'left gonad length') as left_gonad_length,
+    ConcatAttributevalue(flat.collection_object_id,'ovum') as ovum,
+    ConcatAttributevalue(flat.collection_object_id,'molt condition') as molt_condition,
+    ConcatAttributevalue(flat.collection_object_id,'bill length') as bill_length,
+    ConcatAttributevalue(flat.collection_object_id,'bill width') as bill_width,
+    ConcatAttributevalue(flat.collection_object_id,'bill depth') as bill_depth,
+    ConcatAttributevalue(flat.collection_object_id,'head length') as head_length,
+    ConcatAttributevalue(flat.collection_object_id,'tail length') as tail_length,
+    ConcatAttributevalue(flat.collection_object_id,'tarsus length') as tarsus_length,
+    ConcatAttributevalue(flat.collection_object_id,'total length') as total_length,
+    ConcatAttributevalue(flat.collection_object_id,'wing chord') as wing_chord,
+    ConcatAttributevalue(flat.collection_object_id,'weight') as weight,
+    ConcatAttributevalue(flat.collection_object_id,'fat deposition') as fat_deposition,
+    ConcatAttributevalue(flat.collection_object_id,'skull ossification') as skull_ossification,
+    ConcatAttributevalue(flat.collection_object_id,'stomach contents') as stomach_contents,
+    ConcatAttributevalue(flat.collection_object_id,'extension') as extension,
+    ConcatAttributevalue(flat.collection_object_id,'reproductive data') as reproductive_data,
+    concatsingleotherid(flat.collection_object_id,'preparator number') as preparator_number,
+    concatsingleotherid(flat.collection_object_id,'original identifier') as original_identifier,
+    concatsingleotherid(flat.collection_object_id,'secondary identifier') as secondary_identifier,
+    concatsingleotherid(flat.collection_object_id,'collector number') as collector_number,
+    habitat,
+    DECODE(collection_id,'124','BOT-','130','ENTO-','126','FISH-','131','MALA-','113','MAM-','114','ORN-','115','OOL-','144','TEACH-') || cat_num formatted_cat_num,
+    NVL(SUBSTR(identification_remarks, 0, INSTR(identification_remarks, '.')-1),identification_remarks) common_name,
+    RELATEDCATALOGEDITEMS,
+    NVL(SUBSTR(RELATEDCATALOGEDITEMS, INSTR(RELATEDCATALOGEDITEMS, '/CHAS:')+6, 100), RELATEDCATALOGEDITEMS) relationships
 
+FROM
+    flat
+WHERE
+    flat.collection_object_id IN (#collection_object_id#)
+ORDER BY
+    scientific_name,cat_num
+
+--This is test SQL in an attempt to reorder itens in the list to get double sided tags.
 SELECT * FROM (
   SELECT
       scientific_name,
