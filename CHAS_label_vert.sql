c17794d38ff590b8d64ad2f8696a2bcf6b0435f8
anna-chinn <achinn@naturemuseum.org>
2019-07-19 11:00:26 -0500

    report updates, add parts_distinct

--- CHAS_label_vert.sql
+++ CHAS_label_vert.sql
@@ -1,70 +1,65 @@
 -- This is used in CHAS_label_vert
 SELECT
-    scientific_name,
-    cat_num,
-    ACCESSION,
-    higher_geog,
-    SPEC_LOCALITY,
-    flat.VERBATIM_LOCALITY,
-    flat.COLLECTING_METHOD,
-    flat.COLLECTING_SOURCE,
-    flat.BEGAN_DATE,
-    flat.ENDED_DATE,
-    flat.VERBATIM_DATE,
-    COLL_EVENT_REMARKS,
-    round(dec_lat,5) dec_lat,
-    round(dec_long,5) dec_long,
-    COLLECTORS,
-    decode(trim(flat.sex),
-      'male','M',
-      'female','F',
-      'male ?','M?',
-      'female ?','F?',
-      '') sex,
-    ATTRIBUTES,
-    PREPARATORS,
-    PARTS,
-    REMARKS,
-    flat.MINIMUM_ELEVATION,
-    flat.ORIG_ELEV_UNITS,
-    flat.DATUM,
-    ConcatAttributevalue(flat.collection_object_id,'verbatim preservation date') as verbatim_preservation_date,
-    ConcatAttributevalue(flat.collection_object_id,'age class') as age_class,
-    ConcatAttributevalue(flat.collection_object_id,'unformatted measurements') as unformatted_measurements,
-    ConcatAttributevalue(flat.collection_object_id,'right gonad width') as right_gonad_width,
-    ConcatAttributevalue(flat.collection_object_id,'left gonad width') as left_gonad_width,
-    ConcatAttributevalue(flat.collection_object_id,'right gonad length') as right_gonad_length,
-    ConcatAttributevalue(flat.collection_object_id,'left gonad length') as left_gonad_length,
-    ConcatAttributevalue(flat.collection_object_id,'ovum') as ovum,
-    ConcatAttributevalue(flat.collection_object_id,'molt condition') as molt_condition,
-    ConcatAttributevalue(flat.collection_object_id,'bill length') as bill_length,
-    ConcatAttributevalue(flat.collection_object_id,'bill width') as bill_width,
-    ConcatAttributevalue(flat.collection_object_id,'bill depth') as bill_depth,
-    ConcatAttributevalue(flat.collection_object_id,'head length') as head_length,
-    ConcatAttributevalue(flat.collection_object_id,'tail length') as tail_length,
-    ConcatAttributevalue(flat.collection_object_id,'tarsus length') as tarsus_length,
-    ConcatAttributevalue(flat.collection_object_id,'total length') as total_length,
-    ConcatAttributevalue(flat.collection_object_id,'wing chord') as wing_chord,
-    ConcatAttributevalue(flat.collection_object_id,'weight') as weight,
-    ConcatAttributevalue(flat.collection_object_id,'fat deposition') as fat_deposition,
-    ConcatAttributevalue(flat.collection_object_id,'skull ossification') as skull_ossification,
-    ConcatAttributevalue(flat.collection_object_id,'stomach contents') as stomach_contents,
-    ConcatAttributevalue(flat.collection_object_id,'extension') as extension,
-    ConcatAttributevalue(flat.collection_object_id,'reproductive data') as reproductive_data,
-    concatsingleotherid(flat.collection_object_id,'preparator number') as preparator_number,
-    concatsingleotherid(flat.collection_object_id,'original identifier') as original_identifier,
-    concatsingleotherid(flat.collection_object_id,'secondary identifier') as secondary_identifier,
-    concatsingleotherid(flat.collection_object_id,'collector number') as collector_number,
-    habitat,
-    DECODE(collection_id,'124','BOT-','130','ENTO-','126','FISH-','131','MALA-','113','MAM-','114','ORN-','115','OOL-','144','TEACH-', '154', 'HERP-') || cat_num formatted_cat_num,
-    NVL(SUBSTR(identification_remarks, 0, INSTR(identification_remarks, '.')-1),identification_remarks) common_name,
-    RELATEDCATALOGEDITEMS,
-    NVL(SUBSTR(RELATEDCATALOGEDITEMS, INSTR(RELATEDCATALOGEDITEMS, '/CHAS:')+1, 100), RELATEDCATALOGEDITEMS) related_to,
-    NVL(SUBSTR(RELATEDCATALOGEDITEMS, INSTR(RELATEDCATALOGEDITEMS, '(')+1, INSTR(RELATEDCATALOGEDITEMS, ')')-2), RELATEDCATALOGEDITEMS) related_how
+  scientific_name,
+  cat_num,
+  ACCESSION,
+  higher_geog,
+  SPEC_LOCALITY,
+  VERBATIM_LOCALITY,
+  COLLECTING_METHOD,
+  COLLECTING_SOURCE,
+  BEGAN_DATE,
+  ENDED_DATE,
+  VERBATIM_DATE,
+  COLL_EVENT_REMARKS,
+  round(dec_lat,5) dec_lat,
+  round(dec_long,5) dec_long,
+  COLLECTORS,
+  decode(trim(flat.sex),
+    'male','M',
+    'female','F',
+    'male ?','M?',
+    'female ?','F?',
+    '') sex,
+  ATTRIBUTES,
+  PREPARATORS,
+  PARTS,
+  REMARKS,
+  flat.MINIMUM_ELEVATION,
+  flat.ORIG_ELEV_UNITS,
+  flat.DATUM,
+  ConcatAttributevalue(flat.collection_object_id,'verbatim preservation date') as verbatim_preservation_date,
+  ConcatAttributevalue(flat.collection_object_id,'age class') as age_class,
+  concatsingleotherid(flat.collection_object_id,'preparator number') as preparator_number,
+  concatsingleotherid(flat.collection_object_id,'original identifier') as original_identifier,
+  concatsingleotherid(flat.collection_object_id,'secondary identifier') as secondary_identifier,
+  concatsingleotherid(flat.collection_object_id,'collector number') as collector_number,
+  habitat,
+  DECODE(collection_id,'124','BOT-','130','ENTO-','126','FISH-','131','MALA-','113','MAM-','114','ORN-','115','OOL-','144','TEACH-', '154', 'HERP-') || cat_num formatted_cat_num,
+  NVL(SUBSTR(identification_remarks, 0, INSTR(identification_remarks, '.')-1),identification_remarks) common_name,
+  RELATEDCATALOGEDITEMS,
+  NVL(SUBSTR(RELATEDCATALOGEDITEMS, INSTR(RELATEDCATALOGEDITEMS, '/CHAS:')+1, 100), RELATEDCATALOGEDITEMS) related_to,
+  NVL(SUBSTR(RELATEDCATALOGEDITEMS, INSTR(RELATEDCATALOGEDITEMS, '(')+1, INSTR(RELATEDCATALOGEDITEMS, ')')-2), RELATEDCATALOGEDITEMS) related_how,
+  parts_distinct
 
 FROM
-    flat
-WHERE
-    flat.collection_object_id IN (#collection_object_id#)
-ORDER BY
-    scientific_name, cat_num
+  flat
+
+-- this section creates column 'parts_distint', a field that lists distinct specimen parts separated by semicolons.
+RIGHT JOIN
+  (SELECT
+    collection_object_id,
+    listagg(parts, '; ') within group (order by parts) as parts_distinct
+  FROM
+    (SELECT distinct
+      collection_object_id,
+      trim(regexp_substr(parts, '[^;]+', 1, level)) parts
+    FROM
+      (SELECT
+        collection_object_id,
+        parts
+      FROM flat
+      WHERE
+        flat.collection_object_id IN (#collection_object_id#))
+    CONNECT BY regexp_substr(parts, '[^;]+', 1, level) is not null)
+  GROUP BY collection_object_id) a on flat.collection_object_id = a.collection_object_ID
